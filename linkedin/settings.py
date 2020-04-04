8cfa25a01119d99b4b17b57cf5bad06d9a541c37
brbelt <gknez1@wp.pl>
2020-04-04 16:48:07 +0200

    Add json data for warsaw.

--- linkedin/settings.py
+++ linkedin/settings.py
@@ -2,5 +2,24 @@ search_keys = {
     "username"         :  "",
     "password"         :  "",
     # data is written to file in working directory as filename
-    "filename"         :  "output.txt"
+    "filename"         :  "output_1.txt"
 }
+
+
+csv_headers = ['position', 'whole_unnormalized_body', 'Industry', 'Seniority Level', 'Employment Type', 'Job Functions', 'Company Name', 'Company Location']
+
+
+cities = [
+    'Warsaw',
+    'LODZ',
+    'Krakow',
+    'Wroclaw',
+    'Poznan',
+    'Gdansk',
+    'Szczecin',
+    'Bydgoszcz',
+    'Katowice',
+    'Bialystok',
+    'Gdynia'
+]
+
