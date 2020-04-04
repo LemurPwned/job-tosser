e9d38c9510e322dfcec6af30bdbc237bed1f73e6
Worek Dawid <dawid.worek@aalto.fi>
2020-04-04 13:02:53 +0300

    udacity with some additional info

--- udacity-scrapper/udacity_scrapper.py
+++ udacity-scrapper/udacity_scrapper.py
@@ -16,7 +16,6 @@ class UdacityScrapper:
     def parse_course_list(self, link):
 
         db = defaultdict(list)
-        res = requests.get(link)
         req = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
 
         with urlopen(req) as url_handl:
@@ -50,8 +49,13 @@ class UdacityScrapper:
                 if all_skills:
                     for skill in all_skills:
                         skill_list.append(skill.text)
-                    if category:
-                        category = category.text.strip()
+                        
+                if category:
+                    category = category.text.strip()
+                    start_pos = category.find('>')
+                    if start_pos >= 0:
+                        stop_pos = category.find('<', beg=start_pos)
+                        category = category[start_pos+1:stop_pos]
 
                 real_link = title.find('a', {'class': 'capitalize'})['href']
                 desc = card.find('div', {'class': 'card__expander'})
@@ -64,6 +68,37 @@ class UdacityScrapper:
                 db['skills'].append(skill_list)
                 db['category'].append(category)
                 db['level'].append(real_course_level.text)
+
+                full_course_url = r'https://www.udacity.com' + real_link
+                print(full_course_url)
+                req = Request(full_course_url, headers={'User-Agent': 'Mozilla/5.0'})
+                with urlopen(req) as url_handl:
+                    html_code = url_handl.read()
+                    soup = BeautifulSoup(html_code, 'html.parser')
+
+                    try:
+                        course_meta_info = soup.find('ir-canonical-degree-info-columns').find('div').find('ul')
+                        elements = course_meta_info.findAll('li')
+                        db['estimated_time'].append( elements[0].find('h5').getText() + ", " + elements[0].find('p').getText() )
+                        try:
+                            db['prerequisites'].append( elements[2].find('h5').getText().replace('and', '') ) # Remove 'and' for more convenient parsing
+                        except IndexError as e:
+                            db['enroll_by'].append('-')
+                            db['prerequisites'].append( elements[1].find('h5').getText().replace('and', '') ) # Remove 'and' for more convenient parsing
+                        else:
+                            db['enroll_by'].append( elements[1].find('h5').getText() )
+                    except AttributeError as e:
+                        # Handle another formats
+                        details = soup.find('div', {'class': 'details__stats'})
+                        if details != None:
+                            db['estimated_time'].append( details.find('div').findAll('div', {'class': 'col'})[1].find('h5').getText() )
+                            db['enroll_by'].append('-')
+                            db['prerequisites'].append( soup.find('div', {'class': 'course-reqs--summary'}).getText() )
+                        else:
+                            db['estimated_time'].append( soup.find('div', {'class': 'details__overview__item ng-star-inserted'}).getText() )
+                            db['enroll_by'].append('-')
+                            db['prerequisites'].append('-')
+
 
         df = pd.DataFrame.from_dict(db)
         df.to_csv(os.path.join(self.data_save, f'res.csv'), index=False)
