62870ea4794bacb8b7b49898e7f697588deee143
pawelkulig1 <pawelkulig1@gmail.com>
2020-04-04 14:46:37 +0200

    so.csv fixed with script

--- so/so_to_csv.py
+++ so/so_to_csv.py
@@ -1,15 +1,24 @@
 import pickle
+import pandas as pd
 
 keys_all = ['Job type', 'Experience level', 'Role', 'Industry', 'Company size', 'Company type']
 
 data = pickle.load(open("so.pkl", "rb"))
 
-str_data = "job_type,experience,role,industry,company_size,company_tipe,tags\n"
+str_data = "job_type,experience,role,industry,company_size,company_type,tags\n"
 for row in data:
     for key in keys_all:
-        str_data += str(row[1][key]) + ","
-    str_data += str(row[0]) + "\n"
+        str_data += '"' + str(row[1][key]) + '"' + ","
+    str_data += '"' + str(row[0]) + "\"\n"
 
 with open("so.csv", "w") as f:
     f.write(str_data)
-        
+
+df = pd.read_csv("so.csv", sep=",")
+print(df.head())
+print(df.describe())
+df = df[df['role'] != None]
+df.drop_duplicates(inplace=True)
+print(df.describe())
+df.to_csv("so.csv")
+
