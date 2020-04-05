bb7aa14d8a6f4d90b764aace732be47cb4778a03
pawelkulig1 <pawelkulig1@gmail.com>
2020-04-05 02:56:08 +0200

    workaround on front

--- app/server.py
+++ app/server.py
@@ -44,6 +44,8 @@ def courses():
         pass
 
     courses = courses_finder.perform_search(skill, 6)
+    if len(courses) % 2 != 0 and len(courses) > 1:
+        courses = courses[:-1]
     return render_template(COURSES_SUBPAGE, courses=courses)
 
 @app.route('/salary_map')
